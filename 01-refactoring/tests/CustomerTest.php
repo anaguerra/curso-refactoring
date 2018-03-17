ddc23de47eae8fd36ea42bad0feb90bccfa5f0ec
Ana Guerra <aguerravega@gmail.com>
2018-03-17 18:21:47 +0100

    01: Empezando con autoload y composer

--- 01-refactoring/tests/CustomerTest.php
+++ 01-refactoring/tests/CustomerTest.php
@@ -1,11 +1,12 @@
 <?php
 
-require_once(dirname(__DIR__).'/Customer.php');
-require_once(dirname(__DIR__).'/Rental.php');
-require_once(dirname(__DIR__).'/Movie.php');
+namespace Refactoring;
 
 
-class CustomerTest extends PHPUnit_Framework_TestCase
+use PHPUnit\Framework\TestCase;
+
+
+class CustomerTest extends TestCase
 {
     public $customer;
 
