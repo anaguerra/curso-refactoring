38a2780c64ca9525e0784867228f2fc21d307ffb
Ana Guerra <aguerravega@gmail.com>
2018-03-17 22:28:38 +0100

    21: Introducido clase Price dentro de Movie

--- 01-refactoring/src/Movie.php
+++ 01-refactoring/src/Movie.php
@@ -2,28 +2,43 @@
 
 namespace Refactoring;
 
+use Refactoring\Price\Children;
+use Refactoring\Price\NewRelease;
+use Refactoring\Price\Price;
+use Refactoring\Price\Regular;
+
 class Movie
 {
     const CHILDRENS = 2;
     const REGULAR = 0;
     const NEW_RELEASE = 1;
-    private $_title;
-    private $_priceCode;
 
+    private $_title;
+
+    /** @var Price */
+    private $_price;
+
+
+    /**
+     * Movie constructor.
+     * @param $title
+     * @param $priceCode
+     */
     public function __construct($title, $priceCode)
     {
         $this->_title = $title;
-        $this->_priceCode = $priceCode;
+        $this->setPrice($priceCode);
     }
+
 
     public function getPriceCode()
     {
-        return $this->_priceCode;
+        return $this->_price->getPriceCode();
     }
 
     public function setPriceCode($arg)
     {
-        $this->_priceCode = $arg;
+        $this->_price = $arg;
     }
 
     public function getTitle()
@@ -88,4 +103,23 @@ class Movie
         return $frequentRenterPoints;
     }
 
+
+    /**
+     * @param $priceCode
+     */
+    private function setPrice($priceCode)
+    {
+        switch ($priceCode) {
+
+            case self::REGULAR:
+                $this->_price = new Regular();
+                break;
+            case self::NEW_RELEASE:
+                $this->_price = new NewRelease();
+                break;
+            case self::CHILDRENS:
+                $this->_price = new Children();
+                break;
+        }
+    }
 }
